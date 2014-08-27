3bab91e15f6c6d084eb601de48964278579de4d3
binhnt <binhnt@smartosc.com>
2014-08-27 17:29:31 +0700

    update

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.set('env', 'production'); // default: development
 
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
