57795b54f47d2978467ea6524a5f25f92e7309b3
binhnt <binhnt@smartosc.com>
2014-09-23 16:33:02 +0700

    f

--- api/TennisFanstatistic/Test.js
+++ api/TennisFanstatistic/Test.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator PC on 9/23/14.
  */
-var crawler = require('./Crawler');
+var crawler = require('./Crawler').Crawler;
 function Test() {
     var self = this;
     self.getCrawl = function(req, res) {
